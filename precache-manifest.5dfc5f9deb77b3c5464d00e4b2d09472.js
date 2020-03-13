f3908beb3f08a376f677e8e876bb627d08804fbd
GURPRIT <tirprug@users.noreply.github.com>
2020-03-13 06:39:47 +0530

    Updates

---
+++ precache-manifest.5dfc5f9deb77b3c5464d00e4b2d09472.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59c410d9c79d180d36bbe2dce590627",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "70f84989c00001be03b2",
+    "url": "/Keeper/static/js/2.cf582562.chunk.js"
+  },
+  {
+    "revision": "96dfc6dc068e3d8784c9",
+    "url": "/Keeper/static/js/main.b5df1d8a.chunk.js"
+  },
+  {
+    "revision": "1c987da6b821e991f537",
+    "url": "/Keeper/static/js/runtime-main.87236d7e.js"
+  }
+]);
